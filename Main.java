6141e9418c6de6364632b69c88ba32baa592ef7a
Rafaelle Duarte <88789598+RafaelleDuarte@users.noreply.github.com>
2023-08-08 17:06:48 -0300

    Add files via upload

---
+++ Main.java
@@ -0,0 +1,12 @@
+public class Main {
+	public static void myReminder (String dailyquote) {
+	    System.out.println (dailyquote  + " Everything I want wants me more" );
+}
+  
+ public static void  main(String[] args) {
+    myReminder("Manifesting:"); 
+     
+     
+     }
+
+}
